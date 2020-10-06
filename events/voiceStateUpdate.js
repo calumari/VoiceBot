9bb33a97d5cb97b47f6376b2b28735a7cc9c111f
Callum <38501234+calumari@users.noreply.github.com>
2020-10-06 23:37:23 +0100

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,42 +1,43 @@
-module.exports = async (client, oldState, newState) => {
+async function handleLeave({ channel, guild }) {
+    if (!guild.managed.includes(channel.id) || !channel.isEmpty(channel.client.config.ignoreBots)) return;
+    if (!guild.me.hasPermission('MANAGE_CHANNELS')) {
+        return guild.sendAlert({
+            embed: {
+                title: 'Oh dear...',
+                description: `I ran into a little trouble while cleaning up a voice channel. Please ensure that I have permission to **Manage Channels**!`,
+            },
+        });
+    }
+
+    await channel.delete();
+    guild.removeManagedChannel(channel.id);
+}
+
+async function handleJoin({ channel, guild, member }) {
+    if (!guild.triggers.includes(channel.id)) return;
+    if (!guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) {
+        return guild.sendAlert({
+            embed: {
+                title: 'Oh dear...',
+                description: `I ran into a little trouble while creating a voice channel for <@${member.id}>. Please ensure that I have permission to **Manage Channels** and **Move Members**!`,
+            },
+        });
+    }
+
+    const managed = await guild.channels.create(`${member.displayName}'s channel`, {
+        type: 'voice',
+        userLimit: channel.userLimit,
+        parent: channel.parentID,
+        permissionOverwrites: [{ id: member.id, allow: ['VIEW_CHANNEL', 'SPEAK', 'MANAGE_CHANNELS'] }],
+    });
+    guild.addManagedChannel(managed, member);
+    member.voice.setChannel(managed);
+}
+
+module.exports = (client, oldState, newState) => {
     try {
-        if (
-            oldState.channel &&
-            oldState.guild.managed.includes(oldState.channel.id) &&
-            oldState.channel.isEmpty(client.config.ignoreBots)
-        ) {
-            if (!oldState.guild.me.hasPermission('MANAGE_CHANNELS')) {
-                return oldState.guild.sendAlert({
-                    embed: {
-                        title: 'Oh dear...',
-                        description: `I ran into a little trouble while cleaning up a voice channel. Please ensure that I have permission to **Manage Channels**!`,
-                    },
-                });
-            }
-
-            await oldState.channel.delete();
-            oldState.guild.removeManagedChannel(oldState.channelID);
-        }
-
-        if (newState.channel && newState.guild.triggers.includes(newState.channel.id)) {
-            if (!newState.guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) {
-                return newState.guild.sendAlert({
-                    embed: {
-                        title: 'Oh dear...',
-                        description: `I ran into a little trouble while creating a voice channel for <@${newState.member.id}>. Please ensure that I have permission to **Manage Channels** and **Move Members**!`,
-                    },
-                });
-            }
-
-            const channel = await newState.guild.channels.create(`${newState.member.displayName}'s channel`, {
-                type: 'voice',
-                userLimit: newState.channel.userLimit,
-                parent: newState.channel.parentID,
-                permissionOverwrites: [{ id: newState.member.id, allow: ['VIEW_CHANNEL', 'SPEAK', 'MANAGE_CHANNELS'] }],
-            });
-            newState.guild.addManagedChannel(channel, newState.member);
-            newState.member.voice.setChannel(channel);
-        }
+        if (oldState.channelID) handleLeave(oldState);
+        if (newState.channelID) handleJoin(newState);
     } catch (err) {
         // todo: explain what happened to user/owner
         console.log(err);
