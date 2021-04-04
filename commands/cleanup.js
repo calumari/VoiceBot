0be14d5a56808bd8ca27dc504102d768c6ea0989
Callum <38501234+calumari@users.noreply.github.com>
2021-04-04 02:06:30 +0100

    Remove constants
    Causes issues, cba to fix rn

--- commands/cleanup.js
+++ commands/cleanup.js
@@ -1,8 +1,8 @@
-const { Permissions, Constants } = require('discord.js');
+const { Permissions } = require('discord.js');
 
 exports.run = (client, message, args) => {
     const parent = message.guild.channels.cache.find(
-        c => c.name === client.config.categoryName && c.type === Constants.ChannelTypes.CATEGORY
+        c => c.name === client.config.categoryName && c.type === 'category'
     );
     if (!parent) return;
     for (const [, channel] of parent.children) {
